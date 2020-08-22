b12ad50cf96f7dfb0911efb4a8aa91a299a632bd
Dhruv <dhruvrawat2611@gmail.com>
2020-08-23 00:55:31 +0530

    fixed get request

--- Back/app.js
+++ Back/app.js
@@ -3,9 +3,9 @@ const app=express();
 const cors=require('cors');
 const bodyparser=require('body-parser');
 const shortrouter=require('./routes/shortener.js');
-const searcher=require('./routes/searcher');
+const searcher=require('./routes/searcher.js');
 const db=require('./database/db');
-
+const url = require('url');
 
 app.use(cors());
 //app.use(bodyparser.urlencoded({extended:false}));
@@ -15,8 +15,13 @@ app.use((req,res,next)=>{
     console.log('in main');
     next();
 });
-app.use('/',searcher);
-app.use('/',shortrouter);
+
+
+app.get('/*',searcher);
+
+
+
+app.post('/',shortrouter);
 
 app.use((req,res,next)=>{
     res.status(404).send('<h1>Not found</h1>');
--- Back/routes/searcher.js
+++ Back/routes/searcher.js
@@ -2,7 +2,7 @@ const express=require('express');
 const url = require('url');
 const router=express.Router();
 
-router.get('/',(req,res,next)=>{
+router.get('/*',(req,res,next)=>{
     const queryObject = url.parse(req.url,true).query;
     console.log(queryObject)
     console.log('getting');
